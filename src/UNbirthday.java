7d15d9345e07539aacce1cb15a8312f51d95fa8c
Ganymede007 <moyhuro@gmail.com>
2017-11-14 19:24:43 -0800

    .gitignore-bin UNbirthday.java-scr

---
+++ src/UNbirthday.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class UNbirthday{
+	
+public static void main(String[] args) {
+	String date = "11/14/17";
+	String birthday = JOptionPane.showInputDialog("When is your birthday?");
+	if(birthday.equals("11/14/17")){
+		JOptionPane.showMessageDialog(null, "Happy Birthday!");
+	}
+		else
+		{	JOptionPane.showMessageDialog(null, "Merry UNbirthday");
+		}
+		
+	}
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
